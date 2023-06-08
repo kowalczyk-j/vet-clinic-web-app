b80744c04de274439f3d03b1032124ea03584908
Kinga <01169249@pw.edu.pl>
2023-06-08 15:21:11 +0200

    Add duration to return of get_all_appointments, get_all_future_appointments, get_all_past_appointments

--- project/src/data_access.py
+++ project/src/data_access.py
@@ -1,7 +1,7 @@
 from models import (animal, appointment, disease, disease_history, employee,
                     employee_schedule, examination, medical_procedure, owner,
                     payment, procedure_appointment, vet, engine)
-from sqlalchemy import select, insert, or_, and_
+from sqlalchemy import select, insert, or_, and_, func
 from sqlalchemy.orm import Session
 from datetime import date, datetime
 
@@ -129,23 +129,52 @@ def get_appointment_by_id(id):
 
 
 def get_all_appointments():
-    stmt = select(appointment)
+    subq = (select(procedure_appointment.c.appointment_id,
+                   func.sec_to_time(
+                       func.sum(
+                           func.time_to_sec(
+                               medical_procedure.c.estimate_time)))
+                   .label("duration"))
+            .join(procedure_appointment)
+            .group_by(procedure_appointment.c.appointment_id)
+            .subquery())
+    stmt = select(appointment, subq.c.duration).join(subq)
     return execute_statement(stmt).all()
 
 
 def get_all_future_appointments():
     current_time = datetime.now().time()
-    stmt = (select(appointment).where(
-            or_(and_(appointment.c.time >= current_time, appointment.c.date == date.today()),
-                appointment.c.date > date.today())))
+    subq = subq = (select(procedure_appointment.c.appointment_id,
+                   func.sec_to_time(
+                       func.sum(
+                           func.time_to_sec(
+                               medical_procedure.c.estimate_time)))
+                   .label("duration"))
+                   .join(procedure_appointment)
+                   .group_by(procedure_appointment.c.appointment_id)
+                   .subquery())
+    stmt = (select(appointment, subq.c.duration).where(
+            or_(and_(appointment.c.time >= current_time,
+                     appointment.c.date == date.today()),
+                appointment.c.date > date.today())).join(subq))
     return execute_statement(stmt).all()
 
 
 def get_all_past_appointments():
     current_time = datetime.now().time()
-    stmt = (select(appointment).where(
-            or_(and_(appointment.c.time < current_time, appointment.c.date == date.today()),
-                appointment.c.date < date.today())))
+    subq = (select(procedure_appointment.c.appointment_id,
+                   func.sec_to_time(
+                       func.sum(
+                           func.time_to_sec(
+                               medical_procedure.c.estimate_time)))
+                   .label("duration"))
+            .join(procedure_appointment)
+            .group_by(procedure_appointment.c.appointment_id)
+            .subquery())
+    stmt = (select(appointment, subq.c.duration).where(
+            or_(and_(appointment.c.time < current_time,
+                     appointment.c.date == date.today()),
+                appointment.c.date < date.today())).join(subq))
     return execute_statement(stmt).all()
 
 
